54e959c686b883c45bd5184cfa98c22f73fdc150
Udara Lakshan <ulakshan061@gmail.com>
2025-06-08 23:39:56 +0530

    Fix role field rendering in EditUser component

--- src/components/EditUser.tsx
+++ src/components/EditUser.tsx
@@ -111,7 +111,7 @@ const EditUser = () => {
               <FormField
                 control={form.control}
                 name="role"
-                render={({ field }) => (
+                render={() => (
                   <FormItem>
                     <FormLabel>Role</FormLabel>
                     <FormControl>
